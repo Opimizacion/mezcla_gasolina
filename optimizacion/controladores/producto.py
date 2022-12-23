c3de2832d8c017e1f3da8177921269f2848ce711
Sunaidy <sunaidy1996@gmail.com>
2022-12-23 12:25:28 -0500

    add estimacion

--- optimizacion/controladores/producto.py
+++ optimizacion/controladores/producto.py
@@ -37,3 +37,19 @@ class MezclaProducto (APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
 
+    def detallesResultantes():
+        # Datos y Estructuras
+        pInt = {'Nvl', 'Np', 'Ref'}
+        pFin = {'G83', 'G90', 'G94'}
+        pFinC = {
+            'G83': {'price': 3300, 'RBNmin': 58.89, 'RVPmax': 0.617498832595756, 'Azufemax': 1000, 'Densidadmin': 0.7200},
+            'G90': {'price': 3500, 'RBNmin': 62.36, 'RVPmax': 0.617498832595756, 'Azufemax': 1000, 'Densidadmin': 0.7200},
+            'G94': {'price': 3746, 'RBNmin': 65.13, 'RVPmax': 0.617498832595756, 'Azufemax': 1000, 'Densidadmin': 0.7200}
+        }    
+        demandaPF = {
+            'G83': {'Min': 0, 'Max': 'M'},
+            'G90': {'Min': 750, 'Max': 'M'},
+            'G94': {'Min': 300, 'Max': 'M'}
+        }
+        
+
